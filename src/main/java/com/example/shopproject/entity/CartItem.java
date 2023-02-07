b4e026ca4ef7397e7c1c54ae2300b8ac5dc824f8
주동호 <100278301+JOODON@users.noreply.github.com>
2023-02-07 10:42:42 +0900

    영속성 전의 들어가기

--- src/main/java/com/example/shopproject/entity/CartItem.java
+++ src/main/java/com/example/shopproject/entity/CartItem.java
@@ -1,4 +1,28 @@
 package com.example.shopproject.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@Table
 public class CartItem {
+    @Id
+    @GeneratedValue
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+    //상품 정보를 알아야하기때문에 매핑을 해줌
+
+    private int Count;
+    //장바구니에 담을 개수
 }
---
+++ src/main/java/com/example/shopproject/entity/Order.java
@@ -0,0 +1,36 @@
+package com.example.shopproject.entity;
+
+import com.example.shopproject.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order{
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order")//주인 찾아주기
+    private List<OrderItem> orderItems=new ArrayList<>();
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
