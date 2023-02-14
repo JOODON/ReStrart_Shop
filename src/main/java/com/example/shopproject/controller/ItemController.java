76dc77d8ffd5db3fac4bd8785a882a9ca86a11dd
주동호 <100278301+JOODON@users.noreply.github.com>
2023-02-14 16:46:24 +0900

    이미지 파일 관리 진행중 머리속에 안들어오는데..

--- src/main/java/com/example/shopproject/controller/ItemController.java
+++ src/main/java/com/example/shopproject/controller/ItemController.java
@@ -8,10 +8,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
+import javax.swing.text.html.parser.Entity;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -26,6 +29,18 @@ public class ItemController {
         model.addAttribute("itemFormDto",new ItemFormDto());
         return "/item/itemForm";
     }
+    @GetMapping(value = "/admin/item/{itemId}")
+    public String itemDtl(@PathVariable("itemId") Long itemId,Model model){
+        try{
+            ItemFormDto itemFormDto=itemService.getItemDtl(itemId);
+            model.addAttribute("itemFormDto",itemFormDto);
+        }catch (EntityNotFoundException e){
+            model.addAttribute("errorMessage","존재하지 않는 상품입니다");
+            return "item/itemForm";
+        }
+        return "item/itemForm";
+    }
+
     @PostMapping(value = "/admin/item/new")
     public String itemNew(@Valid ItemFormDto itemFormDto, BindingResult bindingResult,Model model
     ,@RequestParam("itemImgFile") List<MultipartFile> itemImgFileList){
--- src/main/java/com/example/shopproject/service/ItemService.java
+++ src/main/java/com/example/shopproject/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.example.shopproject.service;
 
 import com.example.shopproject.dto.ItemFormDto;
+import com.example.shopproject.dto.ItemImgDto;
 import com.example.shopproject.entity.Item;
 import com.example.shopproject.entity.ItemImg;
 import com.example.shopproject.repository.ItemImgRepository;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,7 +24,21 @@ public class ItemService {
     private final ItemImgService itemImgService;
 
     private final ItemImgRepository itemImgRepository;
+    @Transactional(readOnly = true)
+    public ItemFormDto getItemDtl(Long itemId){
+        List<ItemImg> itemImgList=itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+        List<ItemImgDto> itemImgDtoList=new ArrayList<>();
+        for (ItemImg itemImg:itemImgList){
+            ItemImgDto itemImgDto=ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
 
+        Item item=itemRepository.findById(itemId).orElseThrow(EntityNotFoundException::new);
+        ItemFormDto itemFormDto=ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+        return itemFormDto;
+
+    }
     public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList)throws  Exception{
         //상품 등록
         Item item=itemFormDto.createItem();
